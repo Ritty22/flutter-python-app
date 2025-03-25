3d91b8cedb03263ca49a94f6c613b8e5b0fd2f80
ritty <ritty.mm.com>
2025-03-26 03:07:37 +0530

    Fix: Remove venv from tracking

--- backend/test_app.py
+++ backend/test_app.py
@@ -1,13 +1,18 @@
 import pytest
 from app import app
 
+# Add a blank line here ⬇️⬇️
+
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
-def test_hello(client):
-    """Test the 'hello' route"""
-    response = client.get('/')
+# Add a blank line here ⬇️⬇️
+
+
+def test_home(client):
+    response = client.get("/")
     assert response.data == b"Hello, World!"
-    assert response.status_code == 200
+
